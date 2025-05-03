e53ad648c148a250e1cda5e946720444bdcbbef8
wognsths <2021122006@yonsei.ac.kr>
2025-05-03 15:49:40 +0900

    Common files

--- app/common/client/client.py
+++ app/common/client/client.py
@@ -1,7 +1,7 @@
 import httpx
 from httpx_sse import connect_sse
 from typing import Any, AsyncIterable
-from common.types import (
+from app.common.types import (
     AgentCard,
     GetTaskRequest,
     SendTaskRequest,
